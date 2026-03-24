37c15b756f7bbc5f121c364973f3068259225245
zuzia <zuzanna.sobczak@student.put.poznan.pl>
2026-03-24 11:39:54 +0100

    dodaj dokumentacje

--- mainwindow.h
+++ mainwindow.h
@@ -1,27 +1,7 @@
-/*#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
-
-private:
-    Ui::MainWindow *ui;
-};
-#endif // MAINWINDOW_H */
-
+///@file mainwindow.h
+/// @brief główne okno aplikacji pingapka.
+/// @autor Zuzanna Sobczak i Katarzyna Staśczak
+///
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -33,27 +13,59 @@ private:
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-
+/**
+ * @class MainWindow
+ * @brief Klasa zarządzająca interfejsem użytkownika oraz logiką testowania połączenia (ping).
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Konstruktor głównego okna.
+     * @param parent Wskaźnik na rodzica okna (domyślnie nullptr).
+     */
     MainWindow(QWidget *parent = nullptr);
+    /**
+     * @brief Destruktor klasy MainWindow. Czyści zasoby UI i pamięć.
+     */
     ~MainWindow();
 
 private slots:
+    /**
+     * @brief Slot wywoływany po kliknięciu przycisku "Gotowe".
+     * Rozpoczyna proces sprawdzania wpisanego adresu.
+     */
     void on_btnGotowe_clicked();
+    /**
+     * @brief Slot wywoływany po kliknięciu przycisku "Wykres".
+     * Generuje i wyświetla wizualizację zebranych danych.
+     */
     void on_btnWykres_clicked();
 
 private:
-    Ui::MainWindow *ui;
-    QVector<double> pingTimes;
-    QNetworkAccessManager *networkManager;
-
+    Ui::MainWindow *ui; ///< Wskaźnik na interfejs użytkownika zaprojektowany w Qt Designerze.
+    QVector<double> pingTimes;///< Kontener przechowujący czasy odpowiedzi (ping) w milisekundach.
+    QNetworkAccessManager *networkManager;///< Zarządca operacji sieciowych.
+    /**
+     * @brief Przeprowadza test opóźnienia (ping) dla podanego adresu IP.
+     * @param ip Ciąg znaków zawierający adres docelowy.
+     */
     void runPing(QString ip);
+    /**
+     * @brief Zapisuje zebrane wyniki testów do pliku tekstowego lub bazy danych.
+     */
     void saveResults();
+    /**
+     * @brief Tworzy kod skryptu w języku Python służący do zaawansowanej analizy danych.
+     * @return QString zawierający pełny kod skryptu Python.
+     */
     QString generatePythonScript();
+    /**
+     * @brief Uruchamia zewnętrzny skrypt Python.
+     * @param script Treść skryptu do wykonania.
+     */
     void runPythonScript(QString script);
 };
 
